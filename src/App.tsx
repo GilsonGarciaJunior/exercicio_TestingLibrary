3b8e193f75cc761186baceec858a5b100daa916d
GilsonGarciaJunior <juniorrodrigues082006@gmail.com>
2026-05-27 20:45:46 -0300

    adicionando a propriedade Test Library

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import styles from './App.module.css';
 function App() {
   return (
     <div className={styles.app}>
-      <Post imageUrl="https://www.orangeboxminiaturas.com.br/img/products/batmovel-1989-figura-batman-em-metal-jada-toys-1-24-jad-98260_1_1000.jpg">
-        Olha só que legal minha miniatura do Batmóvel.
+      <Post imageUrl="https://cdn.awsli.com.br/135x135/2571/2571273/produto/370344617/cs18006-11-76m2t0gega.jpg">
+        Olha só que legal minha miniatura de uma ferrari.
       </Post>
     </div>
   );
